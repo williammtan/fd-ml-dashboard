a46695ae3cda75fd1f2c7da87dfc2e161459e8f2
WilliamTan2704 <william.h.tan27@gmail.com>
2022-01-09 21:22:24 +0700

    add /similar_many/ endpoint

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -7,5 +7,6 @@ app_name = 'models'
 urlpatterns = [
     path('api/indexer/product', csrf_exempt(views.index_products), name='index_products'),
     path('api/similar_product/<int:product_id>', csrf_exempt(views.similar), name='similar_product'),
+    path('api/similar_product_many', csrf_exempt(views.similar_many), name='similar_product_many'),
 ]
 
--- recommendation/views.py
+++ recommendation/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.conf import settings
 from django.http import JsonResponse, HttpResponseBadRequest
 from elasticsearch.exceptions import NotFoundError
+import numpy as np
 import json
 import time
 
@@ -73,6 +74,71 @@ def similar(request, product_id):
                 for hit in res['hits']['hits']
                 if int(hit.get('_id')) != product_id
             ],
+            'took': time.time()-now
+        }, status=200)
+    else:
+        return HttpResponseBadRequest('Must be a GET request')
+
+def similar_many(request):
+    body = json.loads(request.body)
+    now = time.time()
+
+    if request.method == 'GET':
+        product_ids = body.get('product_ids')
+
+        if not product_ids:
+            return HttpResponseBadRequest('body must include the field "product_ids"')
+
+        try:
+            size = int(request.GET.get('size') or settings.DEFAULT_SIZE)
+        except ValueError:
+            return HttpResponseBadRequest('Size must be an integer')
+
+        categories = []
+        product_vecs = []
+
+        for id in product_ids:
+            try:
+                product_res = settings.ES.get(index=settings.ES_INDEX, id=id)
+            except NotFoundError:
+                return HttpResponseNotFound(f"Product ID ({id}) not in settings.ES index")
+            
+
+            product = product_res['_source']
+            product_vec = product['vector']
+
+            categories.append(product['category'])
+            product_vecs.append(product_vec)
+        
+        avg_product_vec = np.mean(product_vecs, axis=0)
+
+        script_query = {
+            "script_score": {
+                "query": {
+                    "bool": {
+                        "should": [
+                            {'match': {"category": c}}
+                            for c in set(categories)
+                        ],
+                        "minimum_should_match" : 1
+                    }
+                },
+                "script": {
+                    "source": "cosineSimilarity(params.query_vector, 'vector') + 1.0",
+                    "params": {"query_vector": avg_product_vec}
+                }
+            }
+        }
+        res = settings.ES.search(index=settings.ES_INDEX, body={"size": size+len(product_ids), "_source": ["_id"], "query": script_query})
+        return JsonResponse({
+            'data' : [
+                {
+                    "id": int(hit.get('_id')),
+                    "similarity": hit.get('_score')
+                }
+                for hit in res['hits']['hits']
+                if int(hit.get('_id')) not in product_ids
+            ][:size],
             'took': time.time()-now
         }, status=200)
     else:
